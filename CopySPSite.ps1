17c54a9dc290ebc832a7c659e942866e9c5b3381
Woongyeol Choi <cwy5847@gmail.com>
2020-07-03 12:55:22 +0800

    Update CopySPSite.ps1

--- CopySPSite.ps1
+++ CopySPSite.ps1
@@ -3,21 +3,23 @@
 #& "C:\Users\Work\Downloads\CopySPSite.ps1" 
 
 #Set the variables
-$tanent = "m365x280502" # Tanent name. i.e. microsoft in https://microsoft.sharepoint.com
-$sourcesite = "PnPSourceSP" # Source site name in address
-$destinationsite = "PnPDestinationSP" # Destination site name in address
+$SourceTanent = "m365x280502" # Tanent name. i.e. microsoft in https://microsoft.sharepoint.com
+$SourceSite = "PnPSourceSP" # Source site name in address
+$DestinationTanent = "m365x280502" # Tanent name. i.e. microsoft in https://microsoft.sharepoint.com
+$DestinationSite = "PnPDestinationSP" # Destination site in address
+$DestinationTitle = "PnPDestinationSP" # Destination site name in address
 
 #1. Connect to Source SharePoint Site
-Connect-PnPOnline -Url https://$tanent.sharepoint.com/sites/$sourcesite -UseWebLogin
+Connect-PnPOnline -Url https://$SourceTanent.sharepoint.com/sites/$SourceSite -UseWebLogin
 
 #2. Backup Site list structure (Create Site template)
-Get-PnPProvisioningTemplate -Out "$sourcesite.xml" -Handlers Lists, Fields -Force
+Get-PnPProvisioningTemplate -Out "$SourceSite.xml" -Handlers Lists, Fields -Force
 
 #3. Create new site (Destination)
-New-PnPSite -Type CommunicationSite -Title $destinationsite -Url https://$tanent.sharepoint.com/sites/$destinationsite
+New-PnPSite -Type CommunicationSite -Title $DestinationTitle -Url https://$DestinationTanent.sharepoint.com/sites/$DestinationSite
 
 #4. Connect to Destination SharePoint Site
-Connect-PnPOnline -Url https://$tanent.sharepoint.com/sites/$destinationsite -UseWebLogin
+Connect-PnPOnline -Url https://$DestinationTanent.sharepoint.com/sites/$DestinationSite -UseWebLogin
 
 #5. Apply template to Destination site
-Apply-PnPProvisioningTemplate -Path "$sourcesite.xml"
+Apply-PnPProvisioningTemplate -Path "$SourceSite.xml"
