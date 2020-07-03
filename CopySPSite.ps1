597de502897f1f65139750b5604109d3035e63ce
Woongyeol Choi <cwy5847@gmail.com>
2020-07-03 12:22:02 +0800

    introduce variables

--- CopySPSite.ps1
+++ CopySPSite.ps1
@@ -2,18 +2,22 @@
 #Set-ExecutionPolicy RemoteSigned
 #& "C:\Users\Work\Downloads\CopySPSite.ps1" 
 
+#Set the variables
+$tanent = "m365x280502" # Tanent name. i.e. microsoft in https://microsoft.sharepoint.com
+$sourcesite = "PnPSourceSP" # Source site name in address
+$destinationsite = "PnPDestinationSP" # Destination site name in address
+
 #1. Connect to Source SharePoint Site
-Connect-PnPOnline -Url https://m365x280502.sharepoint.com/sites/PnPSourceSP -UseWebLogin
+Connect-PnPOnline -Url https://$tanent.sharepoint.com/sites/$sourcesite -UseWebLogin
 
 #2. Backup Site structure (Create Site template)
-Get-PnPProvisioningTemplate -Out SiteTemplate.xml
+Get-PnPProvisioningTemplate -Out "$sourcesite.xml"
 
 #3. Create new site (Destination)
-New-PnPSite -Type CommunicationSite -Title PnPDestinationSP -Url https://m365x280502.sharepoint.com/sites/PnPDestinationSP
+New-PnPSite -Type CommunicationSite -Title $destinationsite -Url https://m365x280502.sharepoint.com/sites/$destinationsite
 
 #4. Connect to Destination SharePoint Site
-Connect-PnPOnline -Url https://m365x280502.sharepoint.com/sites/PnPDestinationSP -UseWebLogin
+Connect-PnPOnline -Url https://$tanent.sharepoint.com/sites/$destinationsite -UseWebLogin
 
 #5. Apply template to Destination site
-Apply-PnPProvisioningTemplate -Path template.xml
-
+Apply-PnPProvisioningTemplate -Path "$sourcesite.xml"
