3de584a9190297017c61d96f6261ccc21d9930a8
Woong <cwy5847@gmail.com>
2021-09-10 10:29:55 +0800

    85 key works in ino keyboard

--- SecondKeyboard/ArduinoPS2/ArduinoPS2-DEV/ArduinoPS2-DEV.ino
+++ SecondKeyboard/ArduinoPS2/ArduinoPS2-DEV/ArduinoPS2-DEV.ino
@@ -49,13 +49,166 @@ if( PS2KeyAdvanced.available( ) )
 
     Serial.println( d);
 
-  if(d == 353 or d == 354 or d == 355 or d == 356 or d == 357 or d == 358 or d == 359 or d == 360 or d == 361 or d == 362 or d == 363 or d == 364 or d == 64 or d == 49 or d == 50 or d == 51 or d == 52 or d == 53 or d == 54 or d == 55 or d == 56 or d == 57 or d == 48 or d == 60 or d == 95 or d == 81 or d == 87 or d == 69 or d == 82 or d == 84 or d == 89 or d == 85 or d == 73 or d == 79 or d == 80 or d == 65 or d == 83 or d == 68 or d == 70 or d == 71 or d == 72 or d == 74 or d == 75 or d == 76 or d == 90 or d == 88 or d == 67 or d == 86 or d == 66 or d == 78 or d == 77 or d == 93 or d == 94 or d == 92 or d == 91 or d == 58 or d == 59 or d == 61 or d == 62 or d == 283 or d == 285 or d == 4355 or d == 16646 or d == 8456 or d == 780 or d == 2314 or d == 287 or d == 1291 or d == 781 or d == 270 or d == 8457 or d == 16647 or d == 286 or d == 284 or d == 260 or d == 258 or d == 6 or d == 281 or d == 282 or d == 273 or d == 274 or d == 275 or d == 276 or d == 279 or d == 277 or d == 280 or d == 278 or d == 257 or d == 47 or d == 46 or d == 45 or d == 32 or d == 33 or d == 34 or d == 35 or d == 36 or d == 37 or d == 38 or d == 39 or d == 40 or d == 41 or d == 42 or d == 44 or d == 43
-){
+    if( // All the rest
+        d == 49 or d == 50 or d == 51 or d == 52 or d == 53 or d == 54 or d == 55 or d == 56 or d == 57 or d == 48 or d == 81 or d == 87 or d == 69 or d == 82 or d == 84 or d == 89 or d == 85 or d == 73 or d == 79 or d == 80 or d == 65 or d == 83 or d == 68 or d == 70 or d == 71 or d == 72 or d == 74 or d == 75 or d == 76 or d == 90 or d == 88 or d == 67 or d == 86 or d == 66 or d == 78 or d == 77 or
+        d == 283 or d == 285 or d == 4355 or d == 16646 or d == 8456 or d == 780 or d == 2314 or d == 287 or d == 1291 or d == 781 or d == 270 or d == 8457 or d == 16647 or d == 286 or d == 284 or d == 260 or d == 258 or d == 6 or
+        
+        d == 257
+    ){
 
-    SendKeyWithF13(d);
+        SendKeyWithF13(d);
+
+    }
+
+
+
+if( // Rest of NumRow.
+       d == 60 or d == 95 or d == 64
+    ){
+
+        switch (d) {
+        case 60:
+            SendKeyWithF13(d -15);
+            break;
+        case 95:
+            SendKeyWithF13(d - 34);
+            break;
+        case 64:
+            SendKeyWithF13(d + 32);
+            break;
+        }
+    }
+
+
+if( // Rest of qwerty keys. Total 8
+        d == 93 or d == 94 or d == 92 or d == 91 or d == 58 or d == 59 or d == 61 or d == 62
+    ){
+
+        switch (d) {
+        case 93:
+            SendKeyWithF13(d -2);
+            break;
+        case 94:
+            SendKeyWithF13(d - 1);
+            break;
+        case 92:
+            SendKeyWithF13(d);
+            break;
+        case 91:
+            SendKeyWithF13(d - 32);
+            break;
+        case 58:
+            SendKeyWithF13(d - 19);
+            break;
+        case 59:
+            SendKeyWithF13(d - 15);
+            break;
+        case 61:
+            SendKeyWithF13(d - 15);
+            break;
+        case 62:
+            SendKeyWithF13(d - 15);
+            break;
+        }
+    }
+
+
+
+
+if( // 6 operator arrow key column
+        d == 281 or d == 282 or d == 273 or d == 274 or d == 275 or d == 276    
+    ){
+
+        switch (d) {
+        case 281:
+            SendKeyWithF13(d -72);
+            break;
+        case 282:
+            SendKeyWithF13(d - 70);
+            break;
+        case 273:
+            SendKeyWithF13(d - 63);
+            break;
+        case 274:
+            SendKeyWithF13(d - 61);
+            break;
+        case 275:
+            SendKeyWithF13(d - 64);
+            break;
+        case 276:
+            SendKeyWithF13(d - 62);
+            break;
+        }
+    }
+
+
+
+
+    if( // Arrow up left
+        d == 279 or d == 277
+    ){
+        SendKeyWithF13(d - 61);
+    }
+
+        if( // Arrow down right
+        d == 280 or d == 278
+    ){
+        SendKeyWithF13(d - 63);
+    }
+
+
+
+
+     if( // Numpad 1 to 9
+        d == 33 or d == 34 or d == 35 or d == 36 or d == 37 or d == 38 or d == 39 or d == 40 or d == 41
+    ){
+
+        SendKeyWithF13(d + 192);
+
+    }
+
+     if( // Numpad rest 7 keys
+        d == 32 or d == 47 or d == 46 or d == 45 or d == 42 or d == 44 or d == 43
+    ){
+
+        switch (d) {
+        case 47:
+            SendKeyWithF13(d + 173);
+            break;
+        case 46:
+            SendKeyWithF13(d + 175);
+            break;
+        case 45:
+            SendKeyWithF13(d + 177);
+            break;
+        case 32:
+            SendKeyWithF13(d + 202);
+            break;
+        case 42:
+            SendKeyWithF13(d + 193);
+            break;
+        case 44:
+            SendKeyWithF13(d + 179);
+            break;
+        case 43:
+            SendKeyWithF13(d + 181);
+            break;
+            
+        }
+
+        
+
+    }
+
+    
+
+    if( // F1 to F12
+        d == 353 or d == 354 or d == 355 or d == 356 or d == 357 or d == 358 or d == 359 or d == 360 or d == 361 or d == 362 or d == 363 or d == 364
+    ){
+
+        SendKeyWithF13(d - 159);
 
   }
-
 
   } // End of if()
 } // End of loop()
--- SecondKeyboard/ArduinoPS2/PowerApps-Ino.ahk
+++ SecondKeyboard/ArduinoPS2/PowerApps-Ino.ahk
@@ -3,10 +3,193 @@
 SendMode Input  ; Recommended for new scripts due to its superior speed and reliability.
 SetWorkingDir %A_ScriptDir%  ; Ensures a consistent starting directory.
 
+
+; Cheatsheet
+; #: Win
+; !: Alt
+; ^: Ctrl
+; +: Shift
+
+
 F13 & F14::
 MsgBox, "F13 - F14"
 Return
 
+
+; Start F1 to F12
+F13 & F1::
+MsgBox, "F13 - F1"
+Return
+
+F13 & F2::
+MsgBox, "F13 - F2"
+Return
+
+F13 & F3::
+MsgBox, "F13 - F3"
+Return
+
+F13 & F4::
+MsgBox, "F13 - F4"
+Return
+
+F13 & F5::
+GoToWebsite("outlook.com")
+Return
+
+F13 & F6::
+GoToWebsite("make.powerapps.com")
+Return
+
+F13 & F7::
+MsgBox, "F13 - F7"
+Return
+
+F13 & F8::
+MsgBox, "F13 - F8"
+Return
+
+F13 & F9::
+MsgBox, "F13 - F9"
+Return
+
+F13 & F10::
+MsgBox, "F13 - F10"
+Return
+
+F13 & F11::
+MsgBox, "F13 - F11"
+Return
+
+F13 & F12::
+MsgBox, "F13 - F12"
+Return
+; End F1 to F12
+
+
+
+
+
+; Start Arrow Column 6 kays
+F13 & Insert::
+MsgBox, "F13 - Insert"
+Return
+
+F13 & Delete::
+MsgBox, "F13 - Delete"
+Return
+
+F13 & Home::
+MsgBox, "F13 - Home"
+Return
+
+F13 & End::
+MsgBox, "F13 - End"
+Return
+
+F13 & PgUp::
+MsgBox, "F13 - PgUp"
+Return
+
+F13 & PgDn::
+MsgBox, "F13 - PgDn"
+Return
+
+; End Arrow Column 6 kays 
+
+
+
+
+; Start Arrow 
+F13 & Up::
+MsgBox, "F13 - Up"
+Return
+
+F13 & Left::
+MsgBox, "F13 - Left"
+Return
+
+F13 & Down::
+MsgBox, "F13 - Down"
+Return
+
+F13 & Right::
+MsgBox, "F13 - Right"
+Return
+
+; End Arrow 
+
+
+
+
+
+
+; Start Numpad
+
+F13 & Numpad1::
+MsgBox, "F13 - Numpad1"
+Return
+
+F13 & Numpad2::
+MsgBox, "F13 - Numpad2"
+Return
+
+F13 & Numpad3::
+MsgBox, "F13 - Numpad3"
+Return
+
+F13 & Numpad4::
+MsgBox, "F13 - Numpad4"
+Return
+
+F13 & Numpad5::
+MsgBox, "F13 - Numpad5"
+Return
+
+F13 & Numpad6::
+MsgBox, "F13 - Numpad6"
+Return
+
+F13 & Numpad7::
+MsgBox, "F13 - Numpad7"
+Return
+
+F13 & Numpad8::
+MsgBox, "F13 - Numpad8"
+Return
+
+F13 & Numpad9::
+MsgBox, "F13 - Numpad9"
+Return
+
+F13 & Numpad0::
+MsgBox, "F13 - Numpad0"
+Return
+
+F13 & NumpadDiv::
+MsgBox, "F13 - NumpadDiv"
+Return
+
+F13 & NumpadMult::
+MsgBox, "F13 - NumpadMult"
+Return
+
+F13 & NumpadSub::
+MsgBox, "F13 - NumpadSub"
+Return
+
+F13 & NumpadDot::
+MsgBox, "F13 - NumpadDot"
+Return
+
+F13 & NumpadAdd::
+MsgBox, "F13 - NumpadAdd"
+Return
+
+F13 & NumpadEnter::
+MsgBox, "F13 - NumpadEnter"
+Return
+; End Numpad
 
 
 F13 & 1::
@@ -56,3 +239,178 @@ Sleep, 50
 Send, Blank()
 Send, {Left}
 Return
+
+F13 & 4::
+FormulaWithBracket("ShowColumns")
+Return
+
+F13 & r::
+FormulaWithBracket("RenameColumns")
+Return
+
+F13 & f::
+FormulaWithBracket("AddColumns")
+Return
+
+F13 & v::
+FormulaWithBracket("DropColumns")
+Return
+
+
+F13 & 5::
+FormulaWithBracket("Select")
+Return
+
+F13 & t::
+FormulaWithBracket("Reset")
+Return
+
+F13 & g::
+FormulaWithBracket("Refresh")
+Return
+
+F13 & b::
+FormulaWithBracket("Record")
+Return
+
+
+F13 & 6::
+FormulaWithBracket("UpdateContext")
+Return
+
+F13 & y::
+FormulaWithBracket("Set")
+Return
+
+F13 & h::
+FormulaWithBracket("Value")
+Return
+
+F13 & n::
+FormulaWithBracket("Text")
+Return
+
+
+F13 & 7::
+FormulaWithBracket("If")
+Return
+
+F13 & u::
+FormulaWithBracket("GUID")
+Return
+
+F13 & j::
+FormulaWithBracket("Remove")
+Return
+
+F13 & m::
+FormulaWithBracket("RemoveIf")
+Return
+
+
+F13 & 8::
+FormulaWithBracket("Navigate")
+Return
+
+F13 & i::
+FormulaWithBracket("Back")
+Return
+
+F13 & k::
+FormulaWithBracket("Distinct")
+Return
+
+F13 & ,::
+FormulaWithBracket("JSON")
+Return
+
+
+F13 & 9::
+FormulaWithBracket("Count")
+Return
+
+F13 & o::
+FormulaWithBracket("CountRows")
+Return
+
+F13 & l::
+FormulaWithBracket("ForAll")
+Return
+
+F13 & .::
+FormulaWithBracket("Sum")
+Return
+
+
+F13 & 0::
+FormulaWithBracket("ClearCollect")
+Return
+
+F13 & p::
+FormulaWithBracket("Collect")
+Return
+
+F13 & SC027::
+FormulaWithBracket("Clear")
+Return
+
+F13 & /::
+FormulaWithBracket("Patch")
+Return
+
+
+
+F13 & -::
+FormulaWithBracket("First")
+Return
+
+F13 & [::
+FormulaWithBracket("Last")
+Return
+
+F13 & '::
+FormulaWithBracket("Sort")
+Return
+
+
+F13 & =::
+MsgBox, "F13 & ="
+
+Return
+
+F13 & ]::
+MsgBox, "F13 & ]"
+
+Return
+
+F13 & \::
+MsgBox, "F13 & \"
+
+Return
+
+F13 & `::
+MsgBox, "F13 & key left to numrow 1"
+
+Return
+
+
+
+
+; use case: FormulaWithBracket("RenameColumns")
+FormulaWithBracket(Formula)
+{
+    Sleep, 50
+	Send %Formula%
+    Send ()
+    Send {Left}
+}
+
+; use case: GoToWebsite("outlook.com")
+GoToWebsite(Address)
+{
+    Sleep, 50
+    Send !d
+    Sleep, 100
+    Send %Address%
+    Send {Enter}
+}
